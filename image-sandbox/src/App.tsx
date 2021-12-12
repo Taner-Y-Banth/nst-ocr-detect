17ae185ba2931123673ec6d932a9d5c9feb855bf
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2021-12-12 10:27:50 -0800

    adds another image to the react app

--- image-sandbox/src/App.tsx
+++ image-sandbox/src/App.tsx
@@ -5,27 +5,34 @@ import { NstrumentaClient } from 'nstrumenta';
 
 function App() {
   const [imageSrc, setImageSrc] = useState<string>()
+  const [jimpSrc, setJimpSrc] = useState<string>()
   const [text, setText] = useState<string>()
 
   useEffect(() => {
 
-      const wsUrlParam = new URLSearchParams(window.location.search).get("wsUrl");
-  
-      const wsUrl = wsUrlParam ? wsUrlParam : "ws://localhost:8088";
-  
-      const nstClient = new NstrumentaClient({
-        apiKey: "",
-        projectId: "",
-        wsUrl,
-      });
-  
+    const wsUrlParam = new URLSearchParams(window.location.search).get("wsUrl");
+
+    const wsUrl = wsUrlParam ? wsUrlParam : "ws://localhost:8088";
+
+    const nstClient = new NstrumentaClient({
+      apiKey: "",
+      projectId: "",
+      wsUrl,
+    });
+
     nstClient.addListener("open", () => {
       console.log('nst client open')
       nstClient.subscribe('ocr', (message) => {
         const blob = new Blob([message], { type: 'image/png' });
-        const src = URL.createObjectURL(blob)
+        const src = URL.createObjectURL(blob);
         console.log(src);
         setImageSrc(src);
+      })
+      nstClient.subscribe('jimp', (grayscale) => {
+        const jimp = new Blob([grayscale], { type: 'image/png' });
+        const src1 = URL.createObjectURL(jimp);
+        console.log(src1);
+        setJimpSrc(src1);
       })
       nstClient.subscribe('images', (message) => {
         console.log(message);
@@ -39,6 +46,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img id="image" src={imageSrc ? imageSrc : logo} className="App-logo" alt="logo" />
+        <img id="image" src={jimpSrc ? jimpSrc : logo} className="App-logo" alt="logo" />
         <p>
           {text}
         </p>
