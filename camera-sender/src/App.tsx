b70705c6977892f99bf45f6603a3dcc2f8173812
nst-t <tyler@nstrumenta.com>
2021-12-24 22:06:24 -0800

    removes img tag prefix and uses atob

--- camera-sender/src/App.tsx
+++ camera-sender/src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Webcam from "react-webcam";
-import './App.css';
-import { NstrumentaClient } from 'nstrumenta';
+import "./App.css";
+import { NstrumentaClient } from "nstrumenta";
 
 const App = () => {
-
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState(null);
   const nstClientRef = React.useRef<NstrumentaClient>(null);
@@ -13,14 +12,20 @@ const App = () => {
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot();
     setImgSrc(imageSrc);
-    nstClientRef.current?.send('preprocessing', imageSrc)
+    const data = imageSrc.split(',')[1];
+    console.log(data);
+    nstClientRef.current?.sendBuffer(
+      "preprocessing",
+      Uint8Array.from(atob(data), (c) => c.charCodeAt(0))
+    );
   }, [webcamRef, setImgSrc]);
 
   React.useEffect(() => {
-
     const wsUrlParam = new URLSearchParams(window.location.search).get("wsUrl");
 
-    const wsUrl = wsUrlParam ? wsUrlParam : window.location.origin.replace('http', 'ws');
+    const wsUrl = wsUrlParam
+      ? wsUrlParam
+      : window.location.origin.replace("http", "ws");
 
     nstClientRef.current = new NstrumentaClient({
       apiKey: "",
@@ -28,22 +33,14 @@ const App = () => {
       wsUrl,
     });
 
-    nstClientRef.current.init()
-  }, [])
+    nstClientRef.current.init();
+  }, []);
 
   return (
     <>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/png"
-      />
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/png" />
       <button onClick={capture}>Capture photo</button>
-      {imgSrc && (
-        <img
-          src={imgSrc}
-        />
-      )}
+      {imgSrc && <img src={imgSrc} />}
     </>
   );
 };
