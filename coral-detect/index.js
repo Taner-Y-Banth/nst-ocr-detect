bfe0fafe7140be8723742bc6c9df7851018d79e7
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2022-01-29 14:51:29 -0800

    adds jimp

--- coral-detect/index.js
+++ coral-detect/index.js
@@ -3,6 +3,7 @@ import minimist from 'minimist';
 import fsPromises from 'fs/promises';
 import ws from 'ws';
 import { NstrumentaClient } from 'nstrumenta';
+import Jimp from 'jimp';
 
 const argv = minimist(process.argv.slice(2));
 const wsUrl = argv.wsUrl;
@@ -16,9 +17,14 @@ const nstClient = new NstrumentaClient({
 nstClient.addListener("open", () => {
     nstClient.subscribe('preprocessing', async(buff) => {
 
-        await fsPromises.writeFile('infile.png', buff);
-        await $`python3 detect_image.py -m ./test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite -l ./test_data/coco_labels.txt -i infile.png -o ../file-sender/images/processed.png`
+        await fsPromises.writeFile('infileone.png', buff);
 
+        Jimp.read("infileone.png", async() => {
+
+            await Image.writeFile("infile.jpg");
+            await $ `python3 detect_image.py -m ./test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite -l ./test_data/coco_labels.txt -i infile.png -o ../file-sender/images/processed.png`
+
+        });
     });
 });
 
