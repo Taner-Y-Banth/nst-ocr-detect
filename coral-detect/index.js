fda5c3c29a43139a5df414bf27c19c7aabe1c903
Taner-Y-Banth <62570956+Taner-Y-Banth@users.noreply.github.com>
2022-01-29 13:16:33 -0800

    adds fsPromises

--- coral-detect/index.js
+++ coral-detect/index.js
@@ -1,5 +1,30 @@
 import { $ } from 'zx'
+import minimist from 'minimist';
+import fsPromises from 'fs/promises';
+import ws from 'ws';
 
-void async function() {
-    await $ `python3 detect_image.py -m /test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite -l /test_data/coco_labels.txt -i /test_data/grace_hopper.bmp -o ../file-sender/images/grace_hopper_processed.png`
-}()
+const argv = minimist(process.argv.slice(2));
+const wsUrl = argv.wsUrl;
+
+const nstClient = new NstrumentaClient({
+    apiKey: "",
+    projectId: "",
+    wsUrl,
+});
+
+nstClient.addListener("open", () => {
+    nstClient.subscribe('preprocessing', async(buff) => {
+
+        await fsPromises.writeFile('infile.png', buff);
+        await $(`python3 detect_image.py -m /test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite -l /test_data/coco_labels.txt -i infile.png -o ../file-sender/images/processed.png`)
+
+    });
+});
+
+nstClient.addListener("open", () => {
+    console.log("websocket opened successfully");
+});
+
+console.log("nstrumenta init");
+
+nstClient.init(ws);
